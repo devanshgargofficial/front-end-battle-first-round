b264bd58c71e381d94f0927cec84fb7f408f0d35
devanshgargofficial <136746503+devanshgargofficial@users.noreply.github.com>
2024-05-16 20:15:47 +0530

    Uploaded updated files

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 document.querySelector('.start-project').addEventListener('click', function() {
     console.log('Start a project button clicked');
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 4,
+        spaceBetween: 30,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        loop: true, // Enable looping if needed
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+    });
+});
